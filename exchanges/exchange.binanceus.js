5a5826e6c5cb188456e5fc83902bbc010cc6a6bb
oh92 <99452028+oh92@users.noreply.github.com>
2023-05-07 04:17:01 -0400

    Update exchange.binanceus.js

--- exchanges/exchange.binanceus.js
+++ exchanges/exchange.binanceus.js
@@ -6,7 +6,7 @@ module.exports = class frostybot_exchange_binanceus extends frostybot_exchange_b
 
     constructor(stub) {
         super(stub);
-        this.stablecoins = ['USD','USDT','BUSD'];          // Stablecoins supported on this exchange
+        this.stablecoins = ['USDT','BUSD'];          // Stablecoins supported on this exchange
         this.order_sizing = 'base';                  // Exchange requires base size for orders
         this.collateral_assets = ['USDT','BUSD'];    // Assets that are used for collateral
         this.balances_market_map = '{currency}/{stablecoin}' // Which market to use to convert non-USD balances to USD
